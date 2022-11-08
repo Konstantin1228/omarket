e7b9ea342270207acd431f1a4d568dbb10d9d5a0
Konstantin1228 <108726101+Konstantin1228@users.noreply.github.com>
2022-11-08 21:51:51 +0300

    translated with the correct server to mockapi

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -4,13 +4,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    databaseURL: "https://heroic-gantry-362610-default-rtdb.firebaseio.com/",
+    apiKey: "AIzaSyCJz5Ic9YyLEqfa5ra0TZeKNAt28nX1vPk",
+    authDomain: "heroic-gantry-362610.firebaseapp.com",
+    databaseURL: "https://heroic-gantry-362610-default-rtdb.firebaseio.com",
+    projectId: "heroic-gantry-362610",
+    storageBucket: "heroic-gantry-362610.appspot.com",
+    messagingSenderId: "5344425166",
+    appId: "1:5344425166:web:7dd69ab950249e31490c9f",
+    measurementId: "G-17SKJZ4Y9K"
 };
 
 const app = initializeApp(firebaseConfig);
